3018da77fec89b3b94173a9cca18013af824f6a0
Lucas Bordeau <bordeau.lucas@gmail.com>
2024-12-22 23:31:06 +0100

    Removed unused code

--- src/modules/tiddlywiki/utils/convertTiddlyWikiJSONToTiddlers.ts
+++ src/modules/tiddlywiki/utils/convertTiddlyWikiJSONToTiddlers.ts
@@ -1,12 +1,9 @@
 import { Tiddler } from 'src/modules/tiddlywiki/types/Tiddler';
-import { convertTiddlerRawJSONToTiddler } from './converTiddlerRawJSONToTiddler';
 
 export function convertTiddlyWikiJSONToTiddlers(
   tiddlyWikiJSON: any[],
 ): Tiddler[] {
   const tiddlerRawJSONArray = tiddlyWikiJSON;
 
-  const tiddlers = tiddlerRawJSONArray.map(convertTiddlerRawJSONToTiddler);
-
-  return tiddlers;
+  return tiddlerRawJSONArray;
 }
